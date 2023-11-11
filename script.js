5881546274f354dc4f70fcf10b5f8dd6c13ca407
ytapiasa <yeisonalexis1998@gmail.com>
2023-11-11 14:35:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-function togglePlayPause() {
-    var playIcon = document.getElementById('play-icon');
-    var pauseIcon = document.getElementById('pause-icon');
 
-    // Toggle display of play and pause icons
+
+
+/* Play button*/
+function togglePlayPause() {
+    let playIcon = document.getElementById('play-icon');
+    let pauseIcon = document.getElementById('pause-icon');
+
+
     if (playIcon.style.display === 'block') {
       playIcon.style.display = 'none';
       pauseIcon.style.display = 'block';
-      // Add logic for play action here
+
       console.log('Play button clicked');
     } else {
       playIcon.style.display = 'block';
       pauseIcon.style.display = 'none';
-      // Add logic for pause action here
+
       console.log('Pause button clicked');
     }
   }
