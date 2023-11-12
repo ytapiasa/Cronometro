cac845fb35038fc61268824fec1b91377045b628
cristiamgomez <cristiam05gomez@gmail.com>
2023-11-12 10:20:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
+let tiempoRestante1;
+let tiempoRestante2;
+let intervalo1;
+let intervalo2;
 
-
-
-/* Play button*/
 function togglePlayPause() {
-    let playIcon = document.getElementById('play-icon');
-    let pauseIcon = document.getElementById('pause-icon');
+    const inputCronometro1 = document.getElementById("cronometro1");
+    const inputCronometro2 = document.getElementById("cronometro2");
 
+    tiempoRestante1 = parseInt(inputCronometro1.value, 10);
+    tiempoRestante2 = parseInt(inputCronometro2.value, 10);
 
-    if (playIcon.style.display === 'block') {
-      playIcon.style.display = 'none';
-      pauseIcon.style.display = 'block';
-
-      console.log('Play button clicked');
-    } else {
-      playIcon.style.display = 'block';
-      pauseIcon.style.display = 'none';
-
-      console.log('Pause button clicked');
+    if (!isNaN(tiempoRestante1) && tiempoRestante1 > 0) {
+        intervalo1 = setInterval(actualizarCronometro1, 1000);
     }
-  }
+
+    if (!isNaN(tiempoRestante2) && tiempoRestante2 > 0) {
+        intervalo2 = setInterval(actualizarCronometro2, 1000);
+    }
+}
+
+function actualizarCronometro1() {
+    const display1 = document.getElementById("cronometro1Display");
+    display1.textContent = `Tiempo restante en cronómetro 1: ${tiempoRestante1} segundos`;
+
+    if (tiempoRestante1 <= 0) {
+        clearInterval(intervalo1);
+    } else {
+        tiempoRestante1--;
+    }
+}
+
+function actualizarCronometro2() {
+    const display2 = document.getElementById("cronometro2Display");
+    display2.textContent = `Tiempo restante en cronómetro 2: ${tiempoRestante2} segundos`;
+
+    if (tiempoRestante2 <= 0) {
+        clearInterval(intervalo2);
+    } else {
+        tiempoRestante2--;
+    }
+}
